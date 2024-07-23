e8ba4440cf346f714ccf4b2d6d8badd67a8ca089
Andrew <ajweaver18@gmail.com>
2024-07-22 21:37:55 -0400

    feat/ item modal styled

--- WTWR/src/components/ItemModal/ItemModal.jsx
+++ WTWR/src/components/ItemModal/ItemModal.jsx
@@ -3,8 +3,8 @@ import './ItemModal.css'
 function ItemModal({ activeModal, handleCloseClick, card }) {
     return (
         <div className={`modal ${activeModal === 'preview' && 'modal_opened'}`}>
-            <div className="modal__content modal__conten_type_image">
-                <button className='modal__close-btn' type='button' onClick={handleCloseClick}></button>
+            <div className="modal__content modal__content_type_image">
+                <button className='modal__close-btn modal__close-btn_type_image' type='button' onClick={handleCloseClick}></button>
                 <img src={card.link} alt={card.name} className="modal__image" />
                 <div className='modal__card-info'>
                     <h3 className="modal__card-title">{card.name}</h3>
